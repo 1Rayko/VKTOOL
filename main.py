46f35490c5e59256186b74c454f72c07409fcd32
sudoreboot <55040629+kotik06@users.noreply.github.com>
2021-09-21 20:07:56 +0700

    Теперь мы cli

---
+++ main.py
@@ -0,0 +1,55 @@
+import click
+import os
+
+@click.command(help="run full version")
+def full():
+    os.system('cd ~/VKTOOL && python n.py')
+
+    
+@click.command(help="run light version")
+def light():
+    os.system('cd ~/VKTOOL && python light.py')
+
+    
+@click.command(help="check and install updates")
+def update():
+    os.system('cd ~/VKTOOL && git pull')
+
+@click.command(help="install requirements, create an alias in .bashrc / .zshrc")
+def install():
+    os.system('pip install vk_api vk colorama Pillow python3_anticaptcha')
+    print("requirements installed")
+    os.system('''echo "alias vktool-cli='python ~/VKTOOL/main.py'" >> ~/.bashrc''')
+    os.system('''echo "alias vktool-cli='python ~/VKTOOL/main.py'" >> ~/.zshrc''')
+    os.system("cd ~ &&  clear")
+    print("Done")
+@click.group()
+def cli():
+    '''
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWNXK00KK0K000KKKKKKK00KXNWWWWWWWW
+WWWWWWKxlcccccccccccccccccccccclxKWWWWWW
+WWWWWKo;lk00000K000KKKKK000K00kc;oKWWWWW
+WWWWWO::kWWWWWWWWWWWWWWWWWWWWWNk::OWWWWW
+WWWWWO::OWWWWWWWWWWWWWWWWWWWWWWO::OWWWWW
+WWWWWO::OWN0kkKNXOdddONWXOkk0NWO::OWWWWW
+WWWWWO::OWNx:,cOXx;,,lKXd;,:kNWO::OWWWWW
+WWWWWO::OWWXx:,:do;,,cdl;;l0NWWO::OWWWWW
+WWWWWO::OWWWNOc;,,,,,,;;,;oKNWWO::OWWWWW
+WWWWWO::OWWWWWKxl:;;;cxxc;;ckNWO::OWWWWW
+WWWWWO::OWWWWWWWNK0O0KNWX0OOKNWO::OWWWWW
+WWWWWO::OWWWWWWWWWWWWWWWWWWWWWWO::OWWWWW
+WWWWW0c;dXNWWWWWWWWWWWWWWWWWWWXx;c0WWWWW
+WWWWWNkc:loddddddddddddddddddol:ckNWWWWW
+WWWWWWNKOxddddddddddddddddddddxOXWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+(https://github.com/kotik06/VKTOOL/)
+'''
+    pass
+
+cli.add_command(update)
+cli.add_command(full)
+cli.add_command(light)
+cli.add_command(install)
+if __name__ == "__main__":
+    cli()
